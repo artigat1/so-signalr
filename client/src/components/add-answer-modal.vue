cd58a25c06505e12fbab1cdb4202721c4ef5bed4
Steve Doggett <steve@orbitalwitness.com>
2020-08-19 13:22:53 +0100

    added ability to view questions and add answers

---
+++ client/src/components/add-answer-modal.vue
@@ -0,0 +1,47 @@
+﻿<template>
+    <b-modal @hidden="onHidden" hide-footer id="addAnswerModal" ref="addAnswerModal" title="Add new Answer">
+        <b-form @reset.prevent="onCancel" @submit.prevent="onSubmit">
+            <b-form-group label="Your Answer:" label-for="answerInput">
+                <b-form-textarea :max-rows="10"
+                                 :rows="6"
+                                 id="answerInput"
+                                 placeholder="Type your answer"
+                                 v-model="form.body" />
+            </b-form-group>
+
+            <button class="btn btn-primary float-right ml-2" type="submit">Submit</button>
+            <button class="btn btn-secondary float-right" type="reset">Cancel</button>
+        </b-form>
+    </b-modal>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                form: {
+                    body: '',
+                },
+                questionId: this.$route.params.id,
+            }
+        },
+        methods: {
+            onSubmit(evt) {
+                this.$http
+                    .post(`api/question/${ this.questionId }/answer`, this.form)
+                    .then(res => {
+                        this.$emit('answer-added', res.data)
+                        this.$refs.addAnswerModal.hide()
+                    })
+            },
+            onCancel(evt) {
+                this.$refs.addAnswerModal.hide()
+            },
+            onHidden() {
+                Object.assign(this.form, {
+                    answer: '',
+                })
+            },
+        },
+    }
+</script>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '@/views/home.vue'
+import QuestionPage from '@/views/question'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
         path: '/',
         name: 'Home',
         component: Home,
+    },
+    {
+        path: '/question/:id',
+        name: 'Question',
+        component: QuestionPage,
     },
 ]
 
